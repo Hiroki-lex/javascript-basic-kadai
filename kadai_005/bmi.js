0990af3646cc6b25b1b890f85321f157faa75f75
Hiroki-lex <hiroki.yamashita@lexcent.co.jp>
2026-05-07 14:18:35 +0900

    kadai_005をアップロード

---
+++ kadai_005/bmi.js
@@ -0,0 +1,11 @@
+// 変数の宣言・値の代入（体重（kg））
+let bodyweight = 68;
+
+// 変数の宣言・値の代入（身長（m））
+let height = 1.7;
+
+// 変数の宣言・値の代入（BMI）
+let bmi = (bodyweight / (height * height))
+
+// コンソールへの出力
+console.log(bmi);
